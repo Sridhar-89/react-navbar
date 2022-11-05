ca3d37aa7587f61e293f1b970e661b41e1fc6a41
sridhar <sridharbodhanapu89@gmail.com>
2022-11-05 22:11:40 +0530

    initial

---
+++ src/Loginpage.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function Login(){
+    return(<>
+    <body>
+    <h2>login form</h2>
+    <form action="feedback_510.html" method="get">
+    <label>Email</label><input type="email" placeholder="Enter Your registered Email-id"/>
+    <label>password</label><input type="Enter Your password" />
+    <input type="SUBMIT" value="login"/>
+    </form>
+    </body>
+    </>)
+}
+export default Login;
